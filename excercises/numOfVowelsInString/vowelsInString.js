79b35944fb843d8be96c51f2a91b15d948e4b211
francis <franrtg96@gmail.com>
2023-04-19 23:07:09 -0600

    initialized exercise to return number of vowels in a string

---
+++ excercises/numOfVowelsInString/vowelsInString.js
@@ -0,0 +1,8 @@
+// Write a function that takes a string 
+// and returns the number of vowels in the string.
+let messageOne = "este es el mensaje a trabajar"
+const countVowelsInString = (someString) => {
+
+    let numOfVowels 
+
+}

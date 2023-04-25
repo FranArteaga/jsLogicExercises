53acbba5b1eaec50eb6f3b7894413a3969a20741
francis <franrtg96@gmail.com>
2023-04-25 15:49:13 -0600

    solved num of vowels in string

--- excercises/numOfVowelsInString/vowelsInString.js
+++ excercises/numOfVowelsInString/vowelsInString.js
@@ -1,8 +1,24 @@
 // Write a function that takes a string 
 // and returns the number of vowels in the string.
-let messageOne = "este es el mensaje a trabajar"
 const countVowelsInString = (someString) => {
-
-    let numOfVowels 
-
+    
+    let numOfVowels = 0
+    let vowels = ["a","e","i","o","u"]
+    
+    for(let i = 0; i < someString.length; i++){
+        
+        if(vowels.includes(someString[i])){
+            numOfVowels +=1
+             
+        }
+    }
+    return numOfVowels
+    
 }
+let messageOne = "message"
+console.log(countVowelsInString(messageOne))
+
+
+//cómo capitalizar primera letra
+// let messageTwo = messageOne.charAt(0).toLocaleUpperCase() + messageOne.slice(1)
+// console.log(messageTwo)

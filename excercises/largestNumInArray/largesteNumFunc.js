5123bf9a69bc175cb3ea92e1073a2a0790648174
francis <franrtg96@gmail.com>
2023-05-01 17:19:37 -0600

    finished function that returns largest num in array

--- excercises/largestNumInArray/largesteNumFunc.js
+++ excercises/largestNumInArray/largesteNumFunc.js
@@ -1,6 +1,15 @@
 //Write a function that takes an array of numbers 
 //and returns the largest number in the array.
 
-const largestNumFunc = () => {
-    
+arrOfNums = [5, 10, 12, 7, 18, 13]
+const largestNumFunc = (arr) => {
+    let largestNum = 0
+    for (let i = 0; i < arr.length; i++) {
+        if (largestNum <= arr[i]) {
+            largestNum = arr[i]
+        }
+    }
+    return console.log("the largest number is:", largestNum)
+
 }
+largestNumFunc(arrOfNums)

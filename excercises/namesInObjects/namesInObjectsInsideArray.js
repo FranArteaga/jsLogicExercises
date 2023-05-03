2e72e22b6cb84bc37271f199ee7572eda328339f
francis <franrtg96@gmail.com>
2023-05-02 20:10:38 -0600

    initialized structure to work on the excercise to return only names in objects inside an array

---
+++ excercises/namesInObjects/namesInObjectsInsideArray.js
@@ -0,0 +1,18 @@
+//Write a function that takes an array of objects with a "name" property, 
+//and returns a new array with only the names of the objects.
+
+const arrOfObjects = [
+    {
+        name: 'Alice',
+        age: 29
+    },
+    {
+        name: 'Bob',
+        age: 24
+    },
+    {
+        name: 'Charlie',
+        age: 24
+    },
+]
+

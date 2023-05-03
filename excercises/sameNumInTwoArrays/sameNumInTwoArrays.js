888fb2647866bba541a600e3ec7509519d1373ac
francis <franrtg96@gmail.com>
2023-05-02 22:08:30 -0600

    finished function that returns matching numbers from two arrays

--- excercises/sameNumInTwoArrays/sameNumInTwoArrays.js
+++ excercises/sameNumInTwoArrays/sameNumInTwoArrays.js
@@ -1,5 +1,18 @@
 //Write a function that takes two arrays of numbers 
 //and returns a new array with only the numbers that appear in both arrays.
 
-arrofNumsOne = [7, 1, 12, 3, 15, 31, 10]
-arrofNumsTwo = [12, 13, 15, 42, 1]
+arrOfNumsOne = [7, 1, 12, 3, 15, 31, 10]
+arrOfNumsTwo = [12, 13, 15, 42, 1]
+
+const matchingNumbersInArrays = (arrayOne, arrayTwo) => {
+    let arrOfMatchingNums = []
+    arrayOne.forEach(number1 => {
+        arrayTwo.forEach(number2 => {
+            if (number2 === number1) {
+                arrOfMatchingNums.push(number2)
+            }
+        })
+    })
+    return arrOfMatchingNums
+}
+console.log(matchingNumbersInArrays(arrOfNumsOne, arrOfNumsTwo))

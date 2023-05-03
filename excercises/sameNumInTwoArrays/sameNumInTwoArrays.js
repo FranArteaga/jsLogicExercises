934f0a6b99928a061ec4ae138f0e49036ab12b11
francis <franrtg96@gmail.com>
2023-05-02 21:28:25 -0600

    initialized structure for function that returns matching numbers from two arrays

---
+++ excercises/sameNumInTwoArrays/sameNumInTwoArrays.js
@@ -0,0 +1,5 @@
+//Write a function that takes two arrays of numbers 
+//and returns a new array with only the numbers that appear in both arrays.
+
+arrofNumsOne = [7, 1, 12, 3, 15, 31, 10]
+arrofNumsTwo = [12, 13, 15, 42, 1]

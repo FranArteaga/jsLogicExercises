739e3cd50ac3900f268dc0a0de7f85cfdbee2a96
francis <franrtg96@gmail.com>
2023-05-02 20:56:26 -0600

    initialized structure to work on the excercise to return numbers doubled

---
+++ excercises/doubledNumbers/doubledNumbers.js
@@ -0,0 +1,8 @@
+//Write a function that takes an array of numbers 
+//and returns a new array with all the numbers doubled.
+
+const arrOfNums = [2, 3 ,6, 1, 3, 12, 7, 15]
+
+const doublingNumsInArray = (someArr) => {
+    
+}

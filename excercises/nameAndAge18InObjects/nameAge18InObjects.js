ad8b03cde48e232230bb37c1484a570264dc6614
francis <franrtg96@gmail.com>
2023-05-03 21:08:34 -0600

    initialized function that returns name and age in objects when user is 18 or older

---
+++ excercises/nameAndAge18InObjects/nameAge18InObjects.js
@@ -0,0 +1,26 @@
+//Write a function that takes an array of objects with "name" and "age" properties, 
+//and returns a new array with only the objects where the person's age is greater than 18.
+
+const citizens = [
+    {
+        name: 'Alice',
+        age: 30
+    },
+    {
+        name: 'Bob',
+        age: 15
+    },
+    {
+        name: 'Charlie',
+        age: 8
+    },
+    {
+        name: 'David',
+        age:26
+    },
+    {
+        name:'Emily',
+        age: 17
+    }
+]
+

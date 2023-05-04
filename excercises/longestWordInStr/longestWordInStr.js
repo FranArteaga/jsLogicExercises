57ca60f3f82274ee692f7f786af7e7e34e68275a
francis <franrtg96@gmail.com>
2023-05-03 21:32:38 -0600

    initialized function that returns longest word in a string

---
+++ excercises/longestWordInStr/longestWordInStr.js
@@ -0,0 +1,8 @@
+//Write a function that takes a string and 
+//returns the longest word in the string.
+
+const longStr = "se supone que este sea un string largo"
+
+const longestWordInStr = (string) => {
+    
+}

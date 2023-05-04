6d7ec8360ed4a1f7190fa60f094220a38a648b46
francis <franrtg96@gmail.com>
2023-05-03 20:53:14 -0600

    added extra step in function to exclude duplicates

--- excercises/sameNumInTwoArrays/sameNumInTwoArrays.js
+++ excercises/sameNumInTwoArrays/sameNumInTwoArrays.js
@@ -1,14 +1,15 @@
 //Write a function that takes two arrays of numbers 
 //and returns a new array with only the numbers that appear in both arrays.
+//extra: make the returned array to exclude duplicated numbers
 
-arrOfNumsOne = [7, 1, 12, 3, 15, 31, 10]
-arrOfNumsTwo = [12, 13, 15, 42, 1]
+arrOfNumsOne = [7, 1, 3, 15, 31, 10,12]
+arrOfNumsTwo = [13, 15, 12, 42, 12, 1]
 
 const matchingNumbersInArrays = (arrayOne, arrayTwo) => {
     let arrOfMatchingNums = []
     arrayOne.forEach(number1 => {
         arrayTwo.forEach(number2 => {
-            if (number2 === number1) {
+            if (number2 === number1 && !arrOfMatchingNums.includes(number2)) {
                 arrOfMatchingNums.push(number2)
             }
         })
